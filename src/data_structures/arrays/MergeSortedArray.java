8019b73773601e13d00996ef3bbf0e93bbb1573b
N D Trimurthulu Bandaru <trimurthulu.bandaru@gmail.com>
2021-09-30 10:47:25 +0530

    return if found in map

--- src/data_structures/arrays/MergeSortedArray.java
+++ src/data_structures/arrays/MergeSortedArray.java
@@ -1,26 +1,29 @@
-package data_structures.arrays;
-
-import java.util.Arrays;
-
-public class MergeSortedArray {
-
-  public static int[] MargeTwoSortedArray(int[] array1, int[] array2) {
-    final int[] mergedArray = new int[array1.length + array2.length];
-    int j = 0, k = 0;
-    for (int i = 0; i < mergedArray.length; i++) {
-      if (j != array1.length && (k == array2.length || array1[j] < array2[k])) {
-        mergedArray[i] = array1[j];
-        j++;
-      } else {
-        mergedArray[i] = array2[k];
-        k++;
-      }
-    }
-    return mergedArray;
+import java.util.Map;
+import java.util.HashMap;
+class Main {
+  public static void main(String[] args) {
+     Character res = findFirstRecChar(new Character[]{'a','b','c','b'});
+     System.out.println("repeating char: "+res);
   }
 
-  public static void main(String[] args) {
-    int[] arr = MargeTwoSortedArray(new int[]{1,1,8,9,10}, new int[]{1,2,8,9,11,15,16});
-    Arrays.stream(arr).forEach(System.out::println);
+  private static Character findFirstRecChar(Character[] arr){
+    Map<Character,Integer> map = new HashMap<Character,Integer>();
+    for(int i=0; i<arr.length;i++){
+      Integer count = map.get(arr[i]);
+      // System.out.println(arr[i] +"-"+count);
+      if(count != null){
+        // map.put(arr[i],++count);
+        return arr[i];
+      }else{
+        map.put(arr[i],1);
+      }
+    }
+
+    // for(int i=0; i<arr.length;i++){
+    //   if(map.get(arr[i]) > 1){
+    //     return arr[i];
+    //   }
+    // }
+    return null;
   }
 }
